b7cab15738909e258d4fb64d4b491cc5ff016eab
puru763 <purvesh152001@.com>
2025-05-11 00:23:39 +0530

    order microservice

---
+++ orderprocessing/src/main/java/com/chainplus/orderprocessing/OrderprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.chainplus.orderprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderprocessingApplication.class, args);
+	}
+
+}
