a87473c5d1a5ccb988405f6e3802d0b6ae32af29
puru763 <purvesh152001@.com>
2025-05-14 09:17:21 +0530

    logistic microservice

---
+++ logistics/src/test/java/com/chainplus/logistics/LogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chainplus.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
